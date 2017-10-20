9ce5e96724f653fd7a7a90a2bec686eb89ac73c4
Steve Haslam <araqnid@googlemail.com>
2017-10-20 12:30:03 +0100

    Configure Jetty to log requests to SLF4J

--- src/main/kotlin/com/timgroup/smileykt/JettyService.kt
+++ src/main/kotlin/com/timgroup/smileykt/JettyService.kt
@@ -6,12 +6,14 @@ import com.timgroup.tucker.info.servlet.ApplicationInformationServlet
 import com.timgroup.tucker.info.status.StatusPageGenerator
 import org.eclipse.jetty.server.NetworkConnector
 import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.Slf4jRequestLog
 import org.eclipse.jetty.servlet.DefaultServlet
 import org.eclipse.jetty.servlet.ServletContextHandler
 import org.eclipse.jetty.servlet.ServletHolder
 
 class JettyService(port: Int, statusPage: StatusPageGenerator) {
     private val server = Server(port).apply {
+        requestLog = Slf4jRequestLog()
         handler = ServletContextHandler().apply {
             val tuckerServlet = ApplicationInformationServlet(statusPage, Stoppable.ALWAYS_STOPPABLE, Health.ALWAYS_HEALTHY)
             addServlet(ServletHolder(tuckerServlet), "/info/*")
