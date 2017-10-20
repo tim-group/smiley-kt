db0c3c57e06604547a91fb72d2880814b323c314
Steve Haslam <steve.haslam@timgroup.com>
2017-10-20 11:41:49 +0100

    Alex: add a servlet to record happiness in a local map

--- src/main/kotlin/com/timgroup/smileykt/JettyService.kt
+++ src/main/kotlin/com/timgroup/smileykt/JettyService.kt
@@ -15,6 +15,7 @@ class JettyService(port: Int, statusPage: StatusPageGenerator) {
             val tuckerServlet = ApplicationInformationServlet(statusPage, Stoppable.ALWAYS_STOPPABLE, Health.ALWAYS_HEALTHY)
             addServlet(ServletHolder(tuckerServlet), "/info/*")
             addServlet(DefaultServlet::class.java, "/*")
+            addServlet(RecordHappinessServlet::class.java, "/record-happiness")
             resourceBase = "src/main/web"
         }
     }
---
+++ src/main/kotlin/com/timgroup/smileykt/RecordHappinessServlet.kt
@@ -0,0 +1,18 @@
+package com.timgroup.smileykt
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class RecordHappinessServlet : HttpServlet() {
+
+    val happinesses = mutableMapOf<String, String>()
+
+    override fun doPost(req: HttpServletRequest, resp: HttpServletResponse) {
+        val email: String = req.getParameter("email") ?: return resp.sendError(400, "Email must be supplied")
+        val happiness: String = req.getParameter("happiness") ?: return resp.sendError(400, "Happiness must be supplied")
+        happinesses[email] = happiness
+        println(happinesses)
+        resp.status = 200
+    }
+}
