020ef75959434bfc9b8d2278f916952173bd673e
Steve Haslam <steve.haslam@timgroup.com>
2017-10-27 11:51:01 +0100

    Fix up servlet integration test to use only ServerRule

--- src/test/kotlin/com/timgroup/smileykt/RecordHappinessServletIntegrationTest.kt
+++ src/test/kotlin/com/timgroup/smileykt/RecordHappinessServletIntegrationTest.kt
@@ -1,19 +1,18 @@
 package com.timgroup.smileykt
 
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.entity.ContentType
-import org.apache.http.util.EntityUtils
-import org.junit.Test
-import kotlin.test.assertEquals
-import org.apache.http.entity.ByteArrayEntity
-import org.apache.http.HttpEntity
 import org.apache.http.NameValuePair
 import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.client.methods.HttpPost
 import org.apache.http.message.BasicNameValuePair
-import java.util.ArrayList
+import org.junit.Rule
+import org.junit.Test
+import java.util.*
+import kotlin.test.assertEquals
 
-class RecordHappinessServletIntegrationTest : IntegrationTest() {
+class RecordHappinessServletIntegrationTest {
+    @get:Rule
+    val server = ServerRule()
+
     @Test
     fun `records happiness`() {
         val request = HttpPost("/record-happiness")
@@ -23,8 +22,8 @@ class RecordHappinessServletIntegrationTest : IntegrationTest() {
         postParameters.add(BasicNameValuePair("happiness", "very happy"))
         request.setEntity(UrlEncodedFormEntity(postParameters, "UTF-8"))
 
-        execute(request)
+        server.execute(request)
 
-        assertEquals(200, response.statusLine.statusCode)
+        assertEquals(200, server.response.statusLine.statusCode)
     }
 }
