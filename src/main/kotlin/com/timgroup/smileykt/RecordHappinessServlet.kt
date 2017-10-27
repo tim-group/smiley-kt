2adb44d3f9b4dbf3d4af8f56185c53141fe72e7b
Steve Haslam <steve.haslam@timgroup.com>
2017-10-27 12:39:49 +0100

    Submit happiness as JSON from client side
    Server doesn't support this yet, though

--- src/main/kotlin/com/timgroup/smileykt/RecordHappinessServlet.kt
+++ src/main/kotlin/com/timgroup/smileykt/RecordHappinessServlet.kt
@@ -9,11 +9,16 @@ class RecordHappinessServlet : HttpServlet() {
     val happinesses = mutableMapOf<String, String>()
 
     override fun doPost(req: HttpServletRequest, resp: HttpServletResponse) {
-        val email: String = req.getParameter("email") ?: return resp.sendError(400, "Email must be supplied")
-        val happiness: String = req.getParameter("happiness") ?: return resp.sendError(400, "Happiness must be supplied")
-        happinesses[email] = happiness
-        println(happinesses)
-        resp.status = 200
+        if (req.contentType.toMimeType() == "application/x-www-form-urlencoded") {
+            val email: String = req.getParameter("email") ?: return resp.sendError(400, "Email must be supplied")
+            val happiness: String = req.getParameter("happiness") ?: return resp.sendError(400, "Happiness must be supplied")
+            happinesses[email] = happiness
+            println(happinesses)
+            resp.status = HttpServletResponse.SC_OK
+        }
+        else {
+            resp.status = HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE
+        }
     }
 
     override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
@@ -23,5 +28,13 @@ class RecordHappinessServlet : HttpServlet() {
                 writer.println("$email $happiness")
             }
         }
+    }
+
+    private fun String.toMimeType(): String {
+        val sepOffset = indexOf(';')
+        if (sepOffset < 0)
+            return toLowerCase()
+        else
+            return substring(0, sepOffset).toLowerCase()
     }
 }
--- src/test/kotlin/com/timgroup/smileykt/RecordHappinessServletIntegrationTest.kt
+++ src/test/kotlin/com/timgroup/smileykt/RecordHappinessServletIntegrationTest.kt
@@ -1,11 +1,15 @@
 package com.timgroup.smileykt
 
 import org.apache.http.HttpEntity
+import org.apache.http.HttpStatus
 import org.apache.http.client.entity.UrlEncodedFormEntity
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.ContentType
+import org.apache.http.entity.StringEntity
 import org.apache.http.message.BasicNameValuePair
 import org.apache.http.util.EntityUtils
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -17,7 +21,7 @@ class RecordHappinessServletIntegrationTest {
     @Test
     fun `gets empty happiness`() {
         server.execute(HttpGet("/happiness")).apply {
-            assertEquals(200, statusLine.statusCode)
+            assertEquals(HttpStatus.SC_OK, statusLine.statusCode)
             assertEquals("", entity.readText())
         }
     }
@@ -29,12 +33,36 @@ class RecordHappinessServletIntegrationTest {
                     "email" to "test@example.com",
                     "happiness" to "very happy").toFormEntity()
         }).apply {
-            assertEquals(200, statusLine.statusCode)
+            assertEquals(HttpStatus.SC_OK, statusLine.statusCode)
         }
 
         server.execute(HttpGet("/happiness")).apply {
-            assertEquals(200, statusLine.statusCode)
+            assertEquals(HttpStatus.SC_OK, statusLine.statusCode)
             assertEquals("test@example.com very happy\n", entity.readText())
+        }
+    }
+
+    @Test
+    @Ignore
+    fun `records happiness by posting JSON`() {
+        server.execute(HttpPost("/happiness").apply {
+            entity = StringEntity("""{"email":"test@example.com", "happiness":"very happy"}""", ContentType.APPLICATION_JSON)
+        }).apply {
+            assertEquals(HttpStatus.SC_OK, statusLine.statusCode)
+        }
+
+        server.execute(HttpGet("/happiness")).apply {
+            assertEquals(HttpStatus.SC_OK, statusLine.statusCode)
+            assertEquals("test@example.com very happy\n", entity.readText())
+        }
+    }
+
+    @Test
+    fun `rejects happiness in unsupported format`() {
+        server.execute(HttpPost("/happiness").apply {
+            entity = StringEntity("""<record-happiness><email>test@example.com</email><happiness>very happy</happiness></record-happiness>""", ContentType.APPLICATION_XML)
+        }).apply {
+            assertEquals(HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE, statusLine.statusCode)
         }
     }
 
