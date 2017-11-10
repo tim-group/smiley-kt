ee63c83aa5393a48c03a468b09f3218a6cdffeaf
Edward Wong <edward.yueshungwong@timgroup.com>
2017-11-10 12:27:09 +0000

    Alex/Edward/SteveH - converting the app to persist to the eventStore.

--- src/main/kotlin/com/timgroup/smileykt/RecordHappinessServlet.kt
+++ src/main/kotlin/com/timgroup/smileykt/RecordHappinessServlet.kt
@@ -3,6 +3,10 @@ package com.timgroup.smileykt
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.timgroup.eventstore.api.EventSource
+import com.timgroup.eventstore.api.NewEvent
+import com.timgroup.eventstore.api.NewEvent.newEvent
+import com.timgroup.eventstore.api.StreamId
+import com.timgroup.eventstore.api.StreamId.streamId
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -11,7 +15,9 @@ data class HappinessObj(val email: String, val happiness: String)
 
 class RecordHappinessServlet(eventSource: EventSource) : HttpServlet() {
 
-    val happinesses = mutableMapOf<String, String>()
+    val eventCategoryReader = eventSource.readCategory()
+    val eventStreamWriter = eventSource.writeStream()
+
     val mapper = jacksonObjectMapper()
 
     override fun doPost(req: HttpServletRequest, resp: HttpServletResponse) {
@@ -19,15 +25,11 @@ class RecordHappinessServlet(eventSource: EventSource) : HttpServlet() {
             "application/x-www-form-urlencoded" -> {
                 val email: String = req.getParameter("email") ?: return resp.sendError(400, "Email must be supplied")
                 val happiness: String = req.getParameter("happiness") ?: return resp.sendError(400, "Happiness must be supplied")
-
                 recordHappiness(HappinessObj(email, happiness))
-
                 resp.status = HttpServletResponse.SC_NO_CONTENT
             }
             "application/json" -> {
-
                 recordHappiness(mapper.readValue(req.inputStream))
-
                 resp.status = HttpServletResponse.SC_NO_CONTENT
             }
             else -> resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE)
@@ -37,9 +39,14 @@ class RecordHappinessServlet(eventSource: EventSource) : HttpServlet() {
     override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
         resp.contentType = "text/plain"
         resp.writer.use { writer ->
-            happinesses.forEach { email, happiness ->
-                writer.println("$email $happiness")
-            }
+            eventCategoryReader.readCategoryForwards("happiness")
+                    .map { resolvedEvent ->
+                        HappinessObj(resolvedEvent.eventRecord().streamId().id(),
+                                String(resolvedEvent.eventRecord().data()))
+                    }
+                    .forEach { (email, happiness) ->
+                        writer.println("$email $happiness")
+                    }
         }
     }
 
@@ -52,7 +59,7 @@ class RecordHappinessServlet(eventSource: EventSource) : HttpServlet() {
     }
 
     private fun recordHappiness(happinessObj: HappinessObj) {
-        happinesses[happinessObj.email] = happinessObj.happiness
-        println(happinesses)
+        eventStreamWriter.write(streamId("happiness", happinessObj.email),
+                listOf(newEvent("HappinessReceived", happinessObj.happiness.toByteArray())))
     }
 }
