2820c4617132db4b27e6cecd3e13a1193ef272c7
Steve Haslam <araqnid@googlemail.com>
2017-11-13 20:36:10 +0000

    Actually rewrite Javascript as Kotlin

--- webui/src/main/kotlin/Main.kt
+++ webui/src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.events.Event
+import org.w3c.xhr.XMLHttpRequest
+import kotlin.browser.document
+import kotlin.js.json
+
 fun main(args: Array<String>) {
-    println("hello world")
+    document.getElementById("submit")!!.addEventListener("click", { e: Event ->
+        e.preventDefault()
+        val email = (document.getElementById("email") as HTMLInputElement).value
+        val happiness = (document.getElementById("happiness") as HTMLInputElement).value
+        val doc = JSON.stringify(json("email" to email, "happiness" to happiness))
+        console.log("submit form", doc);
+        val xhr = XMLHttpRequest()
+        xhr.open("POST", "/happiness")
+        xhr.setRequestHeader("Content-Type", "application/json")
+        xhr.send(doc)
+    })
 }
