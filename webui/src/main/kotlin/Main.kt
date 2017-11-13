9c1729f121eee87a94f377db2ab9e70f034f2ac6
Steve Haslam <araqnid@googlemail.com>
2017-11-13 22:10:55 +0000

    Use a data class rather than json utility

--- webui/src/main/kotlin/Main.kt
+++ webui/src/main/kotlin/Main.kt
@@ -2,18 +2,19 @@ import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.events.Event
 import org.w3c.xhr.XMLHttpRequest
 import kotlin.browser.document
-import kotlin.js.json
 
 fun main(args: Array<String>) {
     document.getElementById("submit")!!.addEventListener("click", { e: Event ->
         e.preventDefault()
         val email = (document.getElementById("email") as HTMLInputElement).value
         val happiness = (document.getElementById("happiness") as HTMLInputElement).value
-        val doc = JSON.stringify(json("email" to email, "happiness" to happiness))
-        console.log("submit form", doc);
+        val data = HappinessData(email, happiness)
+        console.log("submit form", data)
         val xhr = XMLHttpRequest()
         xhr.open("POST", "/happiness")
         xhr.setRequestHeader("Content-Type", "application/json")
-        xhr.send(doc)
+        xhr.send(JSON.stringify(data))
     })
 }
+
+data class HappinessData(val email: String, val happiness: String)
