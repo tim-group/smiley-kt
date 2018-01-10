456733d27c30d3bee3913280664456118084af05
autobumper <autobumper@timgroup.com>
2018-01-10 13:06:56 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1539 -> com.timgroup:eventstore-filesystem:0.0.1540, com.timgroup:eventstore-memory:0.0.1539 -> com.timgroup:eventstore-memory:0.0.1540, com.timgroup:eventstore-api:0.0.1539 -> com.timgroup:eventstore-api:0.0.1540  * changes to com.timgroup:eventstore-filesystem:    * Steve Haslam: SteveH: also allow warning/critical thresholds for max initial replay to be set explicitly    * Steve Haslam: SteveH: allow warning/critical thresholds for staleness to be specified  * changes to com.timgroup:eventstore-memory:    * Steve Haslam: SteveH: also allow warning/critical thresholds for max initial replay to be set explicitly    * Steve Haslam: SteveH: allow warning/critical thresholds for staleness to be specified  * changes to com.timgroup:eventstore-api:    * Steve Haslam: SteveH: also allow warning/critical thresholds for max initial replay to be set explicitly    * Steve Haslam: SteveH: allow warning/critical thresholds for staleness to be specified

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1493") // autobump
     compile("com.timgroup:tim-logger:1.5.1083") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1235") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1539") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1539") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1540") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1540") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:3.2.3")
@@ -77,7 +77,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1235") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1539") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1540") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.4.1")
     testCompile("com.timgroup:clocks-testing:1.0.1080") // autobump
 
