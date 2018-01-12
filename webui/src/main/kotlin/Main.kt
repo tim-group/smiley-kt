d1d3a3dc03dda3e7fb8fe09c8cd2afb35dacf027
Steve Haslam <steve.haslam@timgroup.com>
2018-01-12 15:54:19 +0000

    Allow user to select emotion from a drop-down

--- webui/src/main/kotlin/Main.kt
+++ webui/src/main/kotlin/Main.kt
@@ -3,17 +3,18 @@ import kotlinx.coroutines.experimental.Unconfined
 import kotlinx.coroutines.experimental.launch
 import org.w3c.dom.Document
 import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.HTMLSelectElement
 import org.w3c.dom.events.Event
 import org.w3c.dom.events.EventTarget
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
     document.getElementById("submit")!!.onClick {
+        val data = HappinessData(document.inputElementValue("email"), document.inputElementValue("emotion"))
         try {
-            postJSON("/happiness",
-                    HappinessData(document.inputElementValue("email"), document.inputElementValue("emotion")))
+            postJSON("/happiness", data)
             for (id in listOf("email", "emotion")) {
-                (document.getElementById(id) as HTMLInputElement).value = ""
+                document.resetInputElement(id)
             }
         } catch (e: RuntimeException) {
             console.error("failed to post JSON", e)
@@ -28,6 +29,21 @@ private fun EventTarget.onClick(block: suspend CoroutineScope.() -> Unit) {
     })
 }
 
-private fun Document.inputElementValue(id: String): String = ((getElementById(id) ?: throw IllegalArgumentException("No such element: $id")) as HTMLInputElement).value
+private fun Document.inputElementValue(id: String): String {
+    val element = getElementById(id) ?: throw IllegalArgumentException("No such element: $id")
+    return when (element) {
+        is HTMLInputElement -> element.value
+        is HTMLSelectElement -> element.value
+        else -> throw IllegalArgumentException("Unhandled input element: $id: $element")
+    }
+}
+
+private fun Document.resetInputElement(id: String) {
+    val element = getElementById(id) ?: throw IllegalArgumentException("No such element: $id")
+    when (element) {
+        is HTMLInputElement -> element.value = ""
+        is HTMLSelectElement -> element.value = ""
+    }
+}
 
 data class HappinessData(val email: String, val emotion: String)
