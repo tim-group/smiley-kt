f3384adf085e317918a241eedf5f0d3740afddef
autobumper <autobumper@timgroup.com>
2018-02-13 19:53:33 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1570 -> com.timgroup:eventstore-filesystem:0.0.1571, com.timgroup:eventstore-api:0.0.1570 -> com.timgroup:eventstore-api:0.0.1571, com.timgroup:eventstore-memory:0.0.1570 -> com.timgroup:eventstore-memory:0.0.1571  * changes to com.timgroup:eventstore-filesystem:    * Brian Roberts: Brian: Can now configure socketTimeout in JDBC URL for read only databases.  * changes to com.timgroup:eventstore-api:    * Brian Roberts: Brian: Can now configure socketTimeout in JDBC URL for read only databases.  * changes to com.timgroup:eventstore-memory:    * Brian Roberts: Brian: Can now configure socketTimeout in JDBC URL for read only databases.

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,8 +65,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1496") // autobump
     compile("com.timgroup:tim-logger:1.5.1084") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1235") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1570") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1570") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1571") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1571") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -85,7 +85,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1235") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1570") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1571") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.4.1")
     testCompile("com.timgroup:clocks-testing:1.0.1080") // autobump
 
