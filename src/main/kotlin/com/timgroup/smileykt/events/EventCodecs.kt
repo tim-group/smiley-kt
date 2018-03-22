abf8dbb8164af8509aa4773a8f4c3c98da13fe46
Steve Haslam <steve.haslam@timgroup.com>
2018-03-22 17:33:01 +0000

    SteveH: use sealed class rather than interface

--- src/main/kotlin/com/timgroup/smileykt/events/EventCodecs.kt
+++ src/main/kotlin/com/timgroup/smileykt/events/EventCodecs.kt
@@ -32,6 +32,6 @@ object EventCodecs {
     }
 }
 
-interface Event
-data class HappinessReceived(val email: String, val date: LocalDate, val emotion: Emotion) : Event
-data class InvitationEmailSent(val recipient: String, val date: LocalDate) : Event
+sealed class Event
+data class HappinessReceived(val email: String, val date: LocalDate, val emotion: Emotion) : Event()
+data class InvitationEmailSent(val recipient: String, val date: LocalDate) : Event()
