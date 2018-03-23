e5090616fb30144849abde6eb02f33a634ccf06b
autobumper <autobumper@timgroup.com>
2018-03-23 12:45:48 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1578 -> com.timgroup:eventstore-filesystem:0.0.1579, com.timgroup:eventstore-api:0.0.1578 -> com.timgroup:eventstore-api:0.0.1579, com.timgroup:eventstore-memory:0.0.1578 -> com.timgroup:eventstore-memory:0.0.1579  * unable to get details for changes to com.timgroup:eventstore-filesystem  * unable to get details for changes to com.timgroup:eventstore-api  * unable to get details for changes to com.timgroup:eventstore-memory

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,8 +96,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1496") // autobump
     compile("com.timgroup:tim-logger:1.5.1086") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1237") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1578") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1578") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1579") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1579") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -117,7 +117,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1237") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1578") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1579") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1080") // autobump
 
