4aead1abd3789c4ca48eacd52ce057e07fe6afc5
autobumper <autobumper@timgroup.com>
2018-03-26 14:42:56 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1584 -> com.timgroup:eventstore-filesystem:0.0.1585, com.timgroup:eventstore-api:0.0.1584 -> com.timgroup:eventstore-api:0.0.1585, com.timgroup:eventstore-memory:0.0.1584 -> com.timgroup:eventstore-memory:0.0.1585  * changes to com.timgroup:eventstore-filesystem:    * autobumper: Autobumper: com.timgroup:Tucker:1.0.1496 -> com.timgroup:Tucker:1.0.1497        * no changes in new version of com.timgroup:Tucker  * changes to com.timgroup:eventstore-api:    * autobumper: Autobumper: com.timgroup:Tucker:1.0.1496 -> com.timgroup:Tucker:1.0.1497        * no changes in new version of com.timgroup:Tucker  * changes to com.timgroup:eventstore-memory:    * autobumper: Autobumper: com.timgroup:Tucker:1.0.1496 -> com.timgroup:Tucker:1.0.1497        * no changes in new version of com.timgroup:Tucker

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,8 +105,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1497") // autobump
     compile("com.timgroup:tim-logger:1.5.1086") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1237") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1584") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1584") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1585") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1585") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -126,7 +126,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1237") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1584") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1585") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1080") // autobump
 
