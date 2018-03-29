4b7b08dac44792e7201b8aa09516898784a26fd6
Steve Haslam <steve.haslam@timgroup.com>
2018-03-29 17:30:21 +0100

    SteveH: don't hold on to ingredients of Server as JettyService properties

--- src/main/kotlin/com/timgroup/smileykt/JettyService.kt
+++ src/main/kotlin/com/timgroup/smileykt/JettyService.kt
@@ -35,46 +35,48 @@ class JettyService(port: Int,
                    eventSource: EventSource,
                    metrics: MetricRegistry,
                    frontEndUri: URI) : AbstractIdleService() {
-    private val jacksonObjectMapper = jacksonObjectMapper()
-            .registerModule(JavaTimeModule())
+    private val server = run {
+        val jacksonObjectMapper = jacksonObjectMapper()
+                .registerModule(JavaTimeModule())
 
-    private val threadPool = InstrumentedQueuedThreadPool(metrics).apply {
-        name = "Jetty"
-    }
-
-    private val httpConfiguration = HttpConfiguration().apply {
-        requestHeaderSize = 16384
-    }
-
-    private val server = Server(threadPool).apply {
-        addConnector(ServerConnector(this, InstrumentedConnectionFactory(HttpConnectionFactory(httpConfiguration), metrics.timer("jetty-http.connections"))).apply {
-            this.port = port
-        })
-        requestLog = Slf4jRequestLog().apply {
-            ignorePaths = arrayOf("/info/*", "/favicon.ico")
+        val threadPool = InstrumentedQueuedThreadPool(metrics).apply {
+            name = "Jetty"
         }
-        handler = ServletContextHandler().apply {
-            gzipHandler = GzipHandler()
-            addServlet(ServletHolder(appStatus.createServlet()), "/info/*")
-            addServlet(ServletHolder(DefaultServlet::class.java).apply {
-                setInitParameter("precompressed", "true")
-                setInitParameter("etags", "true")
-            }, "/")
-            addFilter(FilterHolder(Filter30Dispatcher()), "/*", EnumSet.of(DispatcherType.REQUEST))
-            addEventListener(object : ResteasyBootstrap() {
-                override fun contextInitialized(event: ServletContextEvent) {
-                    super.contextInitialized(event)
-                    val deployment = event.servletContext.getAttribute(ResteasyDeployment::class.java.name) as ResteasyDeployment
-                    deployment.providerFactory.register(JacksonJsonProvider(jacksonObjectMapper))
-                    deployment.registry.addSingletonResource(HappinessResources(eventSource, frontEndUri))
-                    deployment.registry.addSingletonResource(MetricsResource(metrics))
-                }
+
+        val httpConfiguration = HttpConfiguration().apply {
+            requestHeaderSize = 16384
+        }
+
+        Server(threadPool).apply {
+            addConnector(ServerConnector(this, InstrumentedConnectionFactory(HttpConnectionFactory(httpConfiguration), metrics.timer("jetty-http.connections"))).apply {
+                this.port = port
             })
-            if (javaClass.getResource("/www/.MANIFEST") != null)
-                baseResource = embeddedResourcesFromManifest("www/", javaClass.classLoader).asDocumentRoot()
-            else
-                resourceBase = "webui/build/web"
-        }.wrapWith(InstrumentedHandler(metrics, "jetty"))
+            requestLog = Slf4jRequestLog().apply {
+                ignorePaths = arrayOf("/info/*", "/favicon.ico")
+            }
+            handler = ServletContextHandler().apply {
+                gzipHandler = GzipHandler()
+                addServlet(ServletHolder(appStatus.createServlet()), "/info/*")
+                addServlet(ServletHolder(DefaultServlet::class.java).apply {
+                    setInitParameter("precompressed", "true")
+                    setInitParameter("etags", "true")
+                }, "/")
+                addFilter(FilterHolder(Filter30Dispatcher()), "/*", EnumSet.of(DispatcherType.REQUEST))
+                addEventListener(object : ResteasyBootstrap() {
+                    override fun contextInitialized(event: ServletContextEvent) {
+                        super.contextInitialized(event)
+                        val deployment = event.servletContext.getAttribute(ResteasyDeployment::class.java.name) as ResteasyDeployment
+                        deployment.providerFactory.register(JacksonJsonProvider(jacksonObjectMapper))
+                        deployment.registry.addSingletonResource(HappinessResources(eventSource, frontEndUri))
+                        deployment.registry.addSingletonResource(MetricsResource(metrics))
+                    }
+                })
+                if (javaClass.getResource("/www/.MANIFEST") != null)
+                    baseResource = embeddedResourcesFromManifest("www/", javaClass.classLoader).asDocumentRoot()
+                else
+                    resourceBase = "webui/build/web"
+            }.wrapWith(InstrumentedHandler(metrics, "jetty"))
+        }
     }
 
     override fun startUp() {
