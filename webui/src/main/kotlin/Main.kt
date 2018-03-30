997153b4af028ffc0b4129142d733f1c53c12f8b
Steve Haslam <araqnid@googlemail.com>
2018-03-30 22:43:44 +0100

    SteveH: update webui code to post today's date in the JSON from the form
    (Today's date as seen by the browser, that is)

--- webui/src/main/kotlin/Main.kt
+++ webui/src/main/kotlin/Main.kt
@@ -9,7 +9,11 @@ import kotlin.browser.document
 
 fun main(args: Array<String>) {
     document.getElementById("submit")?.onClick {
-        val data = HappinessData(document.inputElementValue("email"), document.inputElementValue("emotion"))
+        val data = HappinessData(
+                date = today().toString(),
+                email = document.inputElementValue("email"),
+                emotion = document.inputElementValue("emotion")
+        )
         try {
             postJSON("/happiness", data)
             for (id in listOf("email", "emotion")) {
@@ -20,6 +24,24 @@ fun main(args: Array<String>) {
         }
     }
 }
+
+external interface JsDate {
+    fun getYear(): Int
+    fun getMonth(): Int
+    fun getDate(): Int
+}
+
+data class LocalDate(val year: Int, val month: Int, val day: Int) {
+    override fun toString() = "${year.pad(4)}-${month.pad(2)}-${day.pad(2)}"
+}
+
+fun JsDate.toLocalDate() = LocalDate(
+        year = getYear() + 1900,
+        month = getMonth() + 1,
+        day = getDate()
+)
+
+fun today() = js("new Date()").unsafeCast<JsDate>().toLocalDate()
 
 private fun EventTarget.onClick(block: suspend CoroutineScope.() -> Unit) {
     addEventListener("click", { e: Event ->
@@ -45,4 +67,14 @@ private fun Document.resetInputElement(id: String) {
     }
 }
 
-data class HappinessData(val email: String, val emotion: String)
+fun Int.pad(width: Int): String {
+    return buildString {
+        val number = this@pad.toString()
+        for (n in 0 until (width - number.length)) {
+            append('0')
+        }
+        append(number)
+    }
+}
+
+data class HappinessData(val date: String, val email: String, val emotion: String)
