cc2ad4aa012e4268e00fa4e31bc1982911e5ecc6
Max Pimm <maxmil@gmail.com>
2018-04-08 23:41:39 +0200

    mpimm: Added simplest ajax loader that I could to get happiness from the backend

--- react-web-ui/src/app/App.kt
+++ react-web-ui/src/app/App.kt
@@ -1,6 +1,5 @@
 package app
 
-import kotlinx.html.classes
 import react.*
 import react.dom.*
 import logo.*
@@ -8,36 +7,11 @@ import ticker.*
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
-        table {
-            attrs.classes = setOf("table")
-            attrs.jsStyle {
-                border = "solid black 1px"
-            }
-            thead {
-                tr {
-                    th { +"User" }
-                    th { +"Date" }
-                    th { +"Happiness" }
-                }
-            }
-            tbody {
-                happiness.forEach { h ->
-                    tr {
-                        td { + h.email }
-                        td { + h.date }
-                        td { + h.emotion }
-                    }
-                }
-            }
+        h1 { + "User happiness"}
+        child(HappinessTable::class){
+            attrs.url = "http://office-smileyapp-001.mgmt.lon.net.local:8000/happiness"
         }
     }
 }
 
 fun RBuilder.app() = child(App::class) {}
-
-data class UserHappiness(val email: String, val date: String, val emotion: String)
-
-val happiness = listOf(
-        UserHappiness("xyz@example.com", "2018-04-01", "HAPPY"),
-        UserHappiness("abc@example.com", "2018-04-01", "SAD")
-)
---
+++ react-web-ui/src/app/HappinessTable.kt
@@ -0,0 +1,74 @@
+package app
+
+import react.*
+import react.dom.*
+import kotlinx.html.classes
+
+import org.w3c.fetch.RequestInit
+import org.w3c.xhr.XMLHttpRequest
+
+interface HappinessTableProps : RProps {
+    var url: String
+}
+
+interface HappinessTableState : RState {
+    var loaded: Boolean
+    var happiness: List<UserHappiness>
+}
+
+data class UserHappiness(val date: String, val email: String, val emotion: String)
+
+class HappinessTable(props: HappinessTableProps) : RComponent<HappinessTableProps, HappinessTableState>(props) {
+    override fun HappinessTableState.init(props: HappinessTableProps) {
+        loaded = false
+    }
+
+    override fun RBuilder.render() {
+        if (!state.loaded) {
+            div { +"loading..." }
+        } else {
+            table {
+                attrs.classes = setOf("table")
+                attrs.jsStyle {
+                    border = "solid black 1px"
+                }
+                tbody {
+                    state.happiness.forEach { h ->
+                        tr {
+                            td { +h.email }
+                            td { +h.date }
+                            td { +h.emotion }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun componentDidMount() {
+        load(props.url)
+    }
+
+    private fun load(url: String) {
+        val xhr = XMLHttpRequest().apply {
+            addEventListener("load", {
+                val status = status.toInt()
+                if (status == 200) {
+                    setState {
+                        loaded = true;
+                        happiness = responseText.trim().split("\n").map { it.parseUserHappiness() }
+                    }
+                } else {
+                    throw RuntimeException("$url: status=$status")
+                }
+            })
+        }
+        xhr.open("get", url)
+        xhr.send()
+    }
+
+    fun String.parseUserHappiness(): UserHappiness {
+        val parts = split(" ")
+        return UserHappiness(parts[0], parts[1], parts[2])
+    }
+}
