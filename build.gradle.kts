d5470ddb97ecd6ffcd0235833943aeeab6b72e50
autobumper <autobumper@timgroup.com>
2018-04-18 14:27:50 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1590 -> com.timgroup:eventstore-filesystem:0.0.1591, com.timgroup:eventstore-api:0.0.1590 -> com.timgroup:eventstore-api:0.0.1591, com.timgroup:eventstore-memory:0.0.1590 -> com.timgroup:eventstore-memory:0.0.1591  * changes to com.timgroup:eventstore-filesystem:    * autobumper: Autobumper: com.youdevise:Matchers:0.0.1266 -> com.youdevise:Matchers:0.0.1267        * changes to com.youdevise:Matchers:          * Steve Haslam: SteveH: include underlying matcher's mismatch description from jsonString          * Steve Haslam: SteveH: Gradle 4.6  * changes to com.timgroup:eventstore-api:    * autobumper: Autobumper: com.youdevise:Matchers:0.0.1266 -> com.youdevise:Matchers:0.0.1267        * changes to com.youdevise:Matchers:          * Steve Haslam: SteveH: include underlying matcher's mismatch description from jsonString          * Steve Haslam: SteveH: Gradle 4.6  * changes to com.timgroup:eventstore-memory:    * autobumper: Autobumper: com.youdevise:Matchers:0.0.1266 -> com.youdevise:Matchers:0.0.1267        * changes to com.youdevise:Matchers:          * Steve Haslam: SteveH: include underlying matcher's mismatch description from jsonString          * Steve Haslam: SteveH: Gradle 4.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,8 +109,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1497") // autobump
     compile("com.timgroup:tim-logger:1.5.1086") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1237") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1590") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1590") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1591") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1591") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -132,7 +132,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1237") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1590") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1591") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1080") // autobump
 
