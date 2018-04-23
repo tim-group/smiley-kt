1bf65a70939bcb6887cc02a014157004aea3ce35
autobumper <autobumper@timgroup.com>
2018-04-23 15:59:43 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1596 -> com.timgroup:eventstore-filesystem:0.0.1597, com.timgroup:eventstore-api:0.0.1596 -> com.timgroup:eventstore-api:0.0.1597, com.timgroup:eventstore-memory:0.0.1596 -> com.timgroup:eventstore-memory:0.0.1597  * changes to com.timgroup:eventstore-filesystem:    * autobumper: Autobumper: com.timgroup:tim-structured-events-testing:0.4.1237 -> com.timgroup:tim-structured-events-testing:0.4.1238, com.timgroup:tim-structured-events:0.4.1237 -> com.timgroup:tim-structured-events:0.4.1238        * changes to com.timgroup:tim-structured-events-testing:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1080 -> com.timgroup:clocks-testing:1.0.1081              * unable to get details for changes to com.timgroup:clocks-testing        * changes to com.timgroup:tim-structured-events:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1080 -> com.timgroup:clocks-testing:1.0.1081              * unable to get details for changes to com.timgroup:clocks-testing  * changes to com.timgroup:eventstore-api:    * autobumper: Autobumper: com.timgroup:tim-structured-events-testing:0.4.1237 -> com.timgroup:tim-structured-events-testing:0.4.1238, com.timgroup:tim-structured-events:0.4.1237 -> com.timgroup:tim-structured-events:0.4.1238        * changes to com.timgroup:tim-structured-events-testing:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1080 -> com.timgroup:clocks-testing:1.0.1081              * unable to get details for changes to com.timgroup:clocks-testing        * changes to com.timgroup:tim-structured-events:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1080 -> com.timgroup:clocks-testing:1.0.1081              * unable to get details for changes to com.timgroup:clocks-testing  * changes to com.timgroup:eventstore-memory:    * autobumper: Autobumper: com.timgroup:tim-structured-events-testing:0.4.1237 -> com.timgroup:tim-structured-events-testing:0.4.1238, com.timgroup:tim-structured-events:0.4.1237 -> com.timgroup:tim-structured-events:0.4.1238        * changes to com.timgroup:tim-structured-events-testing:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1080 -> com.timgroup:clocks-testing:1.0.1081              * unable to get details for changes to com.timgroup:clocks-testing        * changes to com.timgroup:tim-structured-events:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1080 -> com.timgroup:clocks-testing:1.0.1081              * unable to get details for changes to com.timgroup:clocks-testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,8 +109,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1498") // autobump
     compile("com.timgroup:tim-logger:1.5.1086") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1238") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1596") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1596") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1597") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1597") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -132,7 +132,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1238") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1596") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1597") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1081") // autobump
 
