99173e23f1b675578117306bf0436af1583cdb7e
autobumper <autobumper@timgroup.com>
2018-04-26 17:01:08 +0000

    Autobumper: com.timgroup:tim-structured-events-testing:0.4.1239 -> com.timgroup:tim-structured-events-testing:0.4.1240, com.timgroup:tim-structured-events:0.4.1239 -> com.timgroup:tim-structured-events:0.4.1240  * changes to com.timgroup:tim-structured-events-testing:    * Steve Haslam: SteveH: include Scala and Kotlin runtime versions automatically    * Steve Haslam: SteveH: use java.vendor.version on JRE 10  * changes to com.timgroup:tim-structured-events:    * Steve Haslam: SteveH: include Scala and Kotlin runtime versions automatically    * Steve Haslam: SteveH: use java.vendor.version on JRE 10

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,7 +108,7 @@ val kotlinCoroutinesVersion by extra("0.22.5")
 dependencies {
     compile("com.timgroup:Tucker:1.0.1500") // autobump
     compile("com.timgroup:tim-logger:1.5.1087") // autobump
-    compile("com.timgroup:tim-structured-events:0.4.1239") // autobump
+    compile("com.timgroup:tim-structured-events:0.4.1240") // autobump
     compile("com.timgroup:eventstore-api:0.0.1601") // autobump
     compile("com.timgroup:eventstore-filesystem:0.0.1601") // autobump
     compile(kotlin("stdlib-jdk8"))
@@ -131,7 +131,7 @@ dependencies {
     testCompile(kotlin("test-junit"))
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
-    testCompile("com.timgroup:tim-structured-events-testing:0.4.1239") // autobump
+    testCompile("com.timgroup:tim-structured-events-testing:0.4.1240") // autobump
     testCompile("com.timgroup:eventstore-memory:0.0.1601") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1081") // autobump
