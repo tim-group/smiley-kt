c7682887e6a79340d6db1ce21fe49e2d33c65c63
mpimm <max.pimm@acuris.com>
2018-04-27 12:24:41 +0100

    Moved root module into a backend module

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,151 +4,21 @@ import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import java.net.URI
 
 plugins {
-    application
-    kotlin("jvm") version "1.2.40"
-    id("com.timgroup.jarmangit") version "1.1.86"
-    id("com.github.johnrengelman.shadow") version "2.0.3"
-    id("com.timgroup.productstore") version "1.0.3"
+    kotlin("jvm") version "1.2.40" apply false
+    id("com.timgroup.jarmangit") version "1.1.86" apply false
+    id("com.github.johnrengelman.shadow") version "2.0.3" apply false
+    id("com.timgroup.productstore") version "1.0.3" apply false
 }
 
 val buildNumber: String? by extra(System.getenv("ORIGINAL_BUILD_NUMBER") ?: System.getenv("BUILD_NUMBER"))
 val githubUrl: URI by extra(URI("https://github.com/tim-group/smiley-kt"))
 
-group = "com.timgroup"
-if (buildNumber != null) version = "1.0.${buildNumber}"
-
-application {
-    mainClassName = "com.timgroup.smileykt.Launcher"
-}
-
-tasks {
-    "run"(JavaExec::class) {
-        args("config.properties")
-    }
-
-    "runShadow"(JavaExec::class) {
-        args("config.properties")
-    }
-
-    "test" {
-        dependsOn(":webui:assembleWeb")
-    }
-
-    "distZip" {
-        enabled = false
-    }
-
-    "distTar" {
-        enabled = false
-    }
-
-    val sourcesJar by creating(Jar::class) {
-        classifier = "sources"
-        from(java.sourceSets["main"].allSource)
-    }
-
-    val shadowJar by getting(ShadowJar::class) {
-        manifest {
-            attributes(mapOf(
-                    "X-Java-Version" to "10"
-            ))
-        }
-        from("webui/build/web") {
-            into("www")
-        }
-        from("webui/build/resource_manifest") {
-            into("www")
-        }
-        dependsOn(":webui:assembleWeb")
-        dependsOn(":webui:resourceManifest")
-    }
-
-    "assemble" {
-        dependsOn(sourcesJar)
-        dependsOn(shadowJar)
-    }
-}
-
-tasks.withType<JavaCompile> {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
-    options.encoding = "UTF-8"
-    options.isIncremental = true
-    options.isDeprecation = true
-    options.compilerArgs.add("-parameters")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
-        freeCompilerArgs += "-Xjsr305=strict"
-    }
-}
-
-tasks.withType<Jar> {
-    manifest {
-        attributes(mapOf(
-                "Implementation-Title" to project.name,
-                "Implementation-Version" to project.version,
-                "Implementation-Vendor" to "TIM Group"
-        ))
-    }
-}
-
-repositories {
-    jcenter()
-    maven(url = "http://repo.net.local/nexus/content/groups/public")
+allprojects {
+    group = "com.timgroup"
+    if (buildNumber != null) version = "1.0.${buildNumber}"
 }
 
 val jacksonVersion by extra("2.9.4")
 val metricsVersion by extra("4.0.2")
 val jettyVersion by extra("9.4.9.v20180320")
 val kotlinCoroutinesVersion by extra("0.22.5")
-
-dependencies {
-    compile("com.timgroup:Tucker:1.0.1500") // autobump
-    compile("com.timgroup:tim-logger:1.5.1087") // autobump
-    compile("com.timgroup:tim-structured-events:0.4.1240") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1602") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1602") // autobump
-    compile(kotlin("stdlib-jdk8"))
-    compile(kotlin("reflect"))
-    compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
-    compile("io.dropwizard.metrics:metrics-jvm:$metricsVersion")
-    compile("io.dropwizard.metrics:metrics-graphite:$metricsVersion")
-    compile("io.dropwizard.metrics:metrics-jetty9:$metricsVersion")
-    compile("org.eclipse.jetty:jetty-server:$jettyVersion")
-    compile("org.eclipse.jetty:jetty-servlet:$jettyVersion")
-    compile("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    compile("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
-    compile("com.fasterxml.jackson.jaxrs:jackson-jaxrs-json-provider:$jacksonVersion")
-    compile("com.google.guava:guava:24.1-jre")
-    compile("org.jboss.resteasy:resteasy-jaxrs:3.1.2.Final")
-    compile("javax.mail:mail:1.4.6")
-    compile("org.apache.commons:commons-compress:1.16.1")
-
-    testCompile(kotlin("test-junit"))
-    testCompile("com.natpryce:hamkrest:1.4.2.2")
-    testCompile("org.araqnid:hamkrest-json:1.0.3")
-    testCompile("com.timgroup:tim-structured-events-testing:0.4.1240") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1602") // autobump
-    testCompile("org.apache.httpcomponents:httpclient:4.5.5")
-    testCompile("com.timgroup:clocks-testing:1.0.1081") // autobump
-
-    runtime("ch.qos.logback:logback-classic:1.2.3")
-}
-
-kotlin {
-    experimental.coroutines = Coroutines.ENABLE
-}
-
-publishing {
-    (publications) {
-        "productStore"(ProductStorePublication::class) {
-            application = "smiley-kt"
-            artifact(tasks["shadowJar"])
-            from(components["java"])
-        }
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,4 @@ pluginManagement {
 
 rootProject.name = "smiley-kt"
 
-include("webui")
+include("webui", "backend")
