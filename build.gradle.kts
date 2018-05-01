a8cc6bdc371bd339c6ce1c0e50c66daf8f2cc90e
Steve Haslam <steve.haslam@timgroup.com>
2018-05-01 17:25:52 +0100

    Kotlin 1.2.41

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import java.net.URI
 
 plugins {
-    kotlin("jvm") version "1.2.40" apply false
+    kotlin("jvm") version "1.2.41" apply false
 
     id("com.timgroup.jarmangit") version "1.1.86" apply false
     id("com.github.johnrengelman.shadow") version "2.0.3" apply false
