22d6d40843d61361a4056d79b70f47cc85b3f46a
autobumper <autobumper@timgroup.com>
2018-05-01 10:08:58 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1603 -> com.timgroup:eventstore-filesystem:0.0.1604, com.timgroup:eventstore-api:0.0.1603 -> com.timgroup:eventstore-api:0.0.1604, com.timgroup:eventstore-memory:0.0.1603 -> com.timgroup:eventstore-memory:0.0.1604  * changes to com.timgroup:eventstore-filesystem:    * Justin Heister: Fatema/Justin: run current event number queries 1by1 to avoid slow large batch query in mysql  * changes to com.timgroup:eventstore-api:    * Justin Heister: Fatema/Justin: run current event number queries 1by1 to avoid slow large batch query in mysql  * changes to com.timgroup:eventstore-memory:    * Justin Heister: Fatema/Justin: run current event number queries 1by1 to avoid slow large batch query in mysql

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -105,8 +105,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1500") // autobump
     compile("com.timgroup:tim-logger:1.5.1087") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1240") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1603") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1603") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1604") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1604") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -130,7 +130,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1240") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1603") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1604") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1081") // autobump
 
