8551b1736b3c0b26a4c2a240a8c5ad4a109308c0
autobumper <autobumper@timgroup.com>
2018-05-10 12:05:45 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1607 -> com.timgroup:eventstore-filesystem:0.0.1608, com.timgroup:eventstore-api:0.0.1607 -> com.timgroup:eventstore-api:0.0.1608, com.timgroup:eventstore-memory:0.0.1607 -> com.timgroup:eventstore-memory:0.0.1608  * changes to com.timgroup:eventstore-filesystem:    * Steve Haslam: SteveH: add cast to Runnable to work around type inference failure on Java9    * Steve Haslam: SteveH: truncate system time to milliseconds as instants are truncated by TimeReader/event cache    * Steve Haslam: SteveH: specify JVM-1.8 target for Scala compilation       Scala 2.11.12 will refuse to call static methods on interfaces without this  * changes to com.timgroup:eventstore-api:    * Steve Haslam: SteveH: add cast to Runnable to work around type inference failure on Java9    * Steve Haslam: SteveH: truncate system time to milliseconds as instants are truncated by TimeReader/event cache    * Steve Haslam: SteveH: specify JVM-1.8 target for Scala compilation       Scala 2.11.12 will refuse to call static methods on interfaces without this  * changes to com.timgroup:eventstore-memory:    * Steve Haslam: SteveH: add cast to Runnable to work around type inference failure on Java9    * Steve Haslam: SteveH: truncate system time to milliseconds as instants are truncated by TimeReader/event cache    * Steve Haslam: SteveH: specify JVM-1.8 target for Scala compilation       Scala 2.11.12 will refuse to call static methods on interfaces without this

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -134,8 +134,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1501") // autobump
     compile("com.timgroup:tim-logger:1.5.1087") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1241") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1607") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1607") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1608") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1608") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -159,7 +159,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1241") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1607") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1608") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1081") // autobump
 
