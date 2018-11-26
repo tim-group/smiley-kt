ad2acde129ccd7278676c4c7a6f28705758278a8
autobumper <autobumper@timgroup.com>
2018-11-26 16:53:10 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1659 -> com.timgroup:eventstore-filesystem:0.0.1660, com.timgroup:eventstore-memory:0.0.1659 -> com.timgroup:eventstore-memory:0.0.1660, com.timgroup:eventstore-api:0.0.1659 -> com.timgroup:eventstore-api:0.0.1660  * changes to com.timgroup:eventstore-filesystem:    * autobumper: Autobumper: com.timgroup:tim-logger:1.5.1091 -> com.timgroup:tim-logger:1.5.1092        * changes to com.timgroup:tim-logger:          * Steve Haslam: SteveH: FilebeatAppender's superclass RollingFileAppender should be an API element  * changes to com.timgroup:eventstore-memory:    * autobumper: Autobumper: com.timgroup:tim-logger:1.5.1091 -> com.timgroup:tim-logger:1.5.1092        * changes to com.timgroup:tim-logger:          * Steve Haslam: SteveH: FilebeatAppender's superclass RollingFileAppender should be an API element  * changes to com.timgroup:eventstore-api:    * autobumper: Autobumper: com.timgroup:tim-logger:1.5.1091 -> com.timgroup:tim-logger:1.5.1092        * changes to com.timgroup:tim-logger:          * Steve Haslam: SteveH: FilebeatAppender's superclass RollingFileAppender should be an API element

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -133,8 +133,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1511") // autobump
     compile("com.timgroup:tim-logger:1.5.1092") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1248") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1659") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1659") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1660") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1660") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -159,7 +159,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1248") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1659") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1660") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1084") // autobump
 
