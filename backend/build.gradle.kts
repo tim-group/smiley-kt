64f63c5fe97f9604413031df025510db496a5dd5
autobumper <autobumper@timgroup.com>
2018-11-27 17:05:16 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1663 -> com.timgroup:eventstore-filesystem:0.0.1664, com.timgroup:tim-structured-events-testing:0.4.1249 -> com.timgroup:tim-structured-events-testing:0.4.1250, com.timgroup:eventstore-api:0.0.1663 -> com.timgroup:eventstore-api:0.0.1664, com.timgroup:tim-structured-events:0.4.1249 -> com.timgroup:tim-structured-events:0.4.1250, com.timgroup:eventstore-memory:0.0.1663 -> com.timgroup:eventstore-memory:0.0.1664  * changes to com.timgroup:eventstore-filesystem:    * autobumper: Autobumper: com.timgroup:tim-structured-events-testing:0.4.1248 -> com.timgroup:tim-structured-events-testing:0.4.1249, com.timgroup:tim-structured-events:0.4.1248 -> com.timgroup:tim-structured-events:0.4.1249        * changes to com.timgroup:tim-structured-events-testing:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1084 -> com.timgroup:clocks-testing:1.0.1087              * changes to com.timgroup:clocks-testing:                * Steve Haslam: SteveH: Gradle 5.0        * changes to com.timgroup:tim-structured-events:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1084 -> com.timgroup:clocks-testing:1.0.1087              * changes to com.timgroup:clocks-testing:                * Steve Haslam: SteveH: Gradle 5.0  * changes to com.timgroup:tim-structured-events-testing:    * Steve Haslam: SteveH: Gradle 5.0  * changes to com.timgroup:eventstore-api:    * autobumper: Autobumper: com.timgroup:tim-structured-events-testing:0.4.1248 -> com.timgroup:tim-structured-events-testing:0.4.1249, com.timgroup:tim-structured-events:0.4.1248 -> com.timgroup:tim-structured-events:0.4.1249        * changes to com.timgroup:tim-structured-events-testing:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1084 -> com.timgroup:clocks-testing:1.0.1087              * changes to com.timgroup:clocks-testing:                * Steve Haslam: SteveH: Gradle 5.0        * changes to com.timgroup:tim-structured-events:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1084 -> com.timgroup:clocks-testing:1.0.1087              * changes to com.timgroup:clocks-testing:                * Steve Haslam: SteveH: Gradle 5.0  * changes to com.timgroup:tim-structured-events:    * Steve Haslam: SteveH: Gradle 5.0  * changes to com.timgroup:eventstore-memory:    * autobumper: Autobumper: com.timgroup:tim-structured-events-testing:0.4.1248 -> com.timgroup:tim-structured-events-testing:0.4.1249, com.timgroup:tim-structured-events:0.4.1248 -> com.timgroup:tim-structured-events:0.4.1249        * changes to com.timgroup:tim-structured-events-testing:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1084 -> com.timgroup:clocks-testing:1.0.1087              * changes to com.timgroup:clocks-testing:                * Steve Haslam: SteveH: Gradle 5.0        * changes to com.timgroup:tim-structured-events:          * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1084 -> com.timgroup:clocks-testing:1.0.1087              * changes to com.timgroup:clocks-testing:                * Steve Haslam: SteveH: Gradle 5.0

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -132,9 +132,9 @@ repositories {
 dependencies {
     compile("com.timgroup:Tucker:1.0.1511") // autobump
     compile("com.timgroup:tim-logger:1.5.1094") // autobump
-    compile("com.timgroup:tim-structured-events:0.4.1249") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1663") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1663") // autobump
+    compile("com.timgroup:tim-structured-events:0.4.1250") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1664") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1664") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -158,8 +158,8 @@ dependencies {
     testCompile(kotlin("test-junit"))
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
-    testCompile("com.timgroup:tim-structured-events-testing:0.4.1249") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1663") // autobump
+    testCompile("com.timgroup:tim-structured-events-testing:0.4.1250") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1664") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1087") // autobump
 
