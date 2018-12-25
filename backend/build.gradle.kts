f348b35f4d690b610a9f2f770cea040656837d0a
autobumper <autobumper@timgroup.com>
2018-12-25 11:11:55 +0000

    Autobumper: com.timgroup:Tucker:1.0.1514 -> com.timgroup:Tucker:1.0.1515  * no changes in new version of com.timgroup:Tucker

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -128,7 +128,7 @@ repositories {
 }
 
 dependencies {
-    compile("com.timgroup:Tucker:1.0.1514") // autobump
+    compile("com.timgroup:Tucker:1.0.1515") // autobump
     compile("com.timgroup:tim-logger:1.5.1094") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1251") // autobump
     compile("com.timgroup:eventstore-api:0.0.1669") // autobump
