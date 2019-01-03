9383f5fec48568b59c431a46cd972697b88080f2
autobumper <autobumper@timgroup.com>
2019-01-03 16:28:49 +0000

    Autobumper: com.timgroup:tim-structured-events-testing:0.4.1253 -> com.timgroup:tim-structured-events-testing:0.4.1254, com.timgroup:tim-structured-events:0.4.1253 -> com.timgroup:tim-structured-events:0.4.1254  * changes to com.timgroup:tim-structured-events-testing:    * Alex Conlin-Oakley: Exclude incompatible json.org dependency coming in via jackson bridge    * Alex Conlin-Oakley: Revert "Alex: revert my recent changes to json serialization"       This reverts commit ed73563c681c72d55d7d52f678fbdb0e8160540f.  * changes to com.timgroup:tim-structured-events:    * Alex Conlin-Oakley: Exclude incompatible json.org dependency coming in via jackson bridge    * Alex Conlin-Oakley: Revert "Alex: revert my recent changes to json serialization"       This reverts commit ed73563c681c72d55d7d52f678fbdb0e8160540f.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -130,7 +130,7 @@ repositories {
 dependencies {
     compile("com.timgroup:Tucker:1.0.1516") // autobump
     compile("com.timgroup:tim-logger:1.5.1094") // autobump
-    compile("com.timgroup:tim-structured-events:0.4.1253") // autobump
+    compile("com.timgroup:tim-structured-events:0.4.1254") // autobump
     compile("com.timgroup:eventstore-api:0.0.1673") // autobump
     compile("com.timgroup:eventstore-filesystem:0.0.1673") // autobump
     compile(kotlin("stdlib-jdk8"))
@@ -156,7 +156,7 @@ dependencies {
     testCompile(kotlin("test-junit"))
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
-    testCompile("com.timgroup:tim-structured-events-testing:0.4.1253") // autobump
+    testCompile("com.timgroup:tim-structured-events-testing:0.4.1254") // autobump
     testCompile("com.timgroup:eventstore-memory:0.0.1673") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1087") // autobump
