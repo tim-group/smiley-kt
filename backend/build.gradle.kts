507c39add1aa23bfad286640ee87f98ecea5a6fd
autobumper <autobumper@timgroup.com>
2019-02-12 14:41:58 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1693 -> com.timgroup:eventstore-filesystem:0.0.1694, com.timgroup:eventstore-api:0.0.1693 -> com.timgroup:eventstore-api:0.0.1694, com.timgroup:eventstore-memory:0.0.1693 -> com.timgroup:eventstore-memory:0.0.1694  * changes to com.timgroup:eventstore-filesystem:    * autobumper: Autobumper: com.timgroup:Tucker:1.0.1521 -> com.timgroup:Tucker:1.0.1522        * changes to com.timgroup:Tucker:          * Steve Haslam: Prune SensuAsyncComponent          * Steve Haslam: Replace anonymous subclasses of Component with supplyReport in tests  * changes to com.timgroup:eventstore-api:    * autobumper: Autobumper: com.timgroup:Tucker:1.0.1521 -> com.timgroup:Tucker:1.0.1522        * changes to com.timgroup:Tucker:          * Steve Haslam: Prune SensuAsyncComponent          * Steve Haslam: Replace anonymous subclasses of Component with supplyReport in tests  * changes to com.timgroup:eventstore-memory:    * autobumper: Autobumper: com.timgroup:Tucker:1.0.1521 -> com.timgroup:Tucker:1.0.1522        * changes to com.timgroup:Tucker:          * Steve Haslam: Prune SensuAsyncComponent          * Steve Haslam: Replace anonymous subclasses of Component with supplyReport in tests

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -133,8 +133,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1522") // autobump
     compile("com.timgroup:tim-logger:1.5.1095") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1258") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1693") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1693") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1694") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1694") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -159,7 +159,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1258") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1693") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1694") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1087") // autobump
 
