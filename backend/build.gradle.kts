39ff60432bb12db81bc94e846ef743894c521ab6
autobumper <autobumper@timgroup.com>
2019-03-19 12:10:31 +0000

    Autobumper: com.timgroup:clocks-testing:1.0.1088 -> com.timgroup:clocks-testing:1.0.1090  * changes to com.timgroup:clocks-testing:    * Steve Haslam: SteveH: Gradle 5.2    * Steve Haslam: SteveH: target JDKs 8 and 11 on Travis    * Steve Haslam: SteveH: ignore IntelliJ output directories    * Steve Haslam: SteveH: settings KTS    * Steve Haslam: SteveH: update publish script to take artifacts from local repo

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -155,7 +155,7 @@ dependencies {
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1259") // autobump
     testCompile("com.timgroup:eventstore-memory:0.0.1705") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
-    testCompile("com.timgroup:clocks-testing:1.0.1088") // autobump
+    testCompile("com.timgroup:clocks-testing:1.0.1090") // autobump
 
     runtime("ch.qos.logback:logback-classic:1.2.3")
 
