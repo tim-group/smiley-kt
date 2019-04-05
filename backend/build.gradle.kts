0eaa085ff9c50f958e33cc2dc4056b6c24860b0d
autobumper <autobumper@timgroup.com>
2019-04-05 08:53:18 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1727 -> com.timgroup:eventstore-filesystem:0.0.1728, com.timgroup:eventstore-api:0.0.1727 -> com.timgroup:eventstore-api:0.0.1728, com.timgroup:eventstore-memory:0.0.1727 -> com.timgroup:eventstore-memory:0.0.1728  * changes to com.timgroup:eventstore-filesystem:    * Justin Heister: Propagate Termination events through the event pipeline. Ensure legacy SubscriptionListener is invoked as before to preserve backwards compatability.    * Justin Heister: Pass subscription lifecycle events down to EventHandler to avoid needing to use SubscriptionListener (since it can result in threading issues)    * Justin Heister: Cleanup  * changes to com.timgroup:eventstore-api:    * Justin Heister: Propagate Termination events through the event pipeline. Ensure legacy SubscriptionListener is invoked as before to preserve backwards compatability.    * Justin Heister: Pass subscription lifecycle events down to EventHandler to avoid needing to use SubscriptionListener (since it can result in threading issues)    * Justin Heister: Cleanup  * changes to com.timgroup:eventstore-memory:    * Justin Heister: Propagate Termination events through the event pipeline. Ensure legacy SubscriptionListener is invoked as before to preserve backwards compatability.    * Justin Heister: Pass subscription lifecycle events down to EventHandler to avoid needing to use SubscriptionListener (since it can result in threading issues)    * Justin Heister: Cleanup

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -128,8 +128,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1522") // autobump
     compile("com.timgroup:tim-logger:1.5.1095") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1260") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1727") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1727") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1728") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1728") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:$metricsVersion")
@@ -153,7 +153,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1260") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1727") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1728") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1090") // autobump
 
