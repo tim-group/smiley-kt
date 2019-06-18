9d7a895d876d1a9d0c670af182c955f3b6b94d7a
autobumper <autobumper@timgroup.com>
2019-06-18 10:44:18 +0000

    Autobumper: com.timgroup:Tucker:1.0.1523 -> com.timgroup:Tucker:1.0.1524  * changes to com.timgroup:Tucker:    * gallan: Allow passing a MetricRegistry that will be rendered on request to /info/metrics.        - formatted as "$name[singlespace]$value\n"        - each metric value is written in a similar format as when written to Graphite (c.f. GraphiteReporter)        - prefix is not included (for now)        - metrics are printed by insertion order (may be worth changing to alphabetical)        - there's no synchronisation around the MetricRegistry, need to check if it and it's metrics are thread-safe        - constructors that don't take a MetricRegistry are @Deprecated        - metrics-core dependency is a conservatively early version. Hoping it's forwards-compatible with 4.0+ versions, but I haven't tested.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -119,7 +119,7 @@ repositories {
 }
 
 dependencies {
-    compile("com.timgroup:Tucker:1.0.1523") // autobump
+    compile("com.timgroup:Tucker:1.0.1524") // autobump
     compile("com.timgroup:tim-logger:1.5.1096") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1263") // autobump
     compile("com.timgroup:eventstore-api:0.0.1773") // autobump
