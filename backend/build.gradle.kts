b4cb710b9f19e4818de349780e18e7ff46d6ff60
autobumper <autobumper@timgroup.com>
2019-08-02 17:12:34 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1797 -> com.timgroup:eventstore-filesystem:0.0.1798, com.timgroup:eventstore-api:0.0.1797 -> com.timgroup:eventstore-api:0.0.1798, com.timgroup:eventstore-memory:0.0.1797 -> com.timgroup:eventstore-memory:0.0.1798  * changes to com.timgroup:eventstore-filesystem:    * Max Pimm: mpimm: Don't cache event store metadata in tucker component       TIM2 swtiches from slave to master and this breaks the caching. In the       case of failover it would also be good to see which was the current connection.  * changes to com.timgroup:eventstore-api:    * Max Pimm: mpimm: Don't cache event store metadata in tucker component       TIM2 swtiches from slave to master and this breaks the caching. In the       case of failover it would also be good to see which was the current connection.  * changes to com.timgroup:eventstore-memory:    * Max Pimm: mpimm: Don't cache event store metadata in tucker component       TIM2 swtiches from slave to master and this breaks the caching. In the       case of failover it would also be good to see which was the current connection.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -122,8 +122,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1530") // autobump
     compile("com.timgroup:tim-logger:1.5.1096") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1265") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1797") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1797") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1798") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1798") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:${Versions.metrics}")
@@ -147,7 +147,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1265") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1797") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1798") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1095") // autobump
 
