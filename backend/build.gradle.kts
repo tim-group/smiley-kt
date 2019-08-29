479d728174d8148b13ad3900aacf598a47da9f98
autobumper <autobumper@timgroup.com>
2019-08-29 09:35:52 +0000

    Autobumper: com.timgroup:Tucker:1.0.1530 -> com.timgroup:Tucker:1.0.1531  * changes to com.timgroup:Tucker:    * Steve Haslam: Build against OpenJDK8 and 11 on Travis

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -119,7 +119,7 @@ repositories {
 }
 
 dependencies {
-    compile("com.timgroup:Tucker:1.0.1530") // autobump
+    compile("com.timgroup:Tucker:1.0.1531") // autobump
     compile("com.timgroup:tim-logger:1.5.1096") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1266") // autobump
     compile("com.timgroup:eventstore-api:0.0.1824") // autobump
