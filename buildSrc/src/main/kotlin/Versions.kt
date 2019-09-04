46f31fcd0674415cc035bd60bd89798f5ca208e0
Steve Haslam <steve.haslam@timgroup.com>
2019-09-04 16:55:25 +0100

    SteveH: upgrade various supporting libraries

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
-    val jackson = "2.9.8"
-    val metrics = "4.0.5"
-    val jetty = "9.4.19.v20190610"
-    val kotlinCoroutines = "1.2.2"
-    val guava = "28.0-jre"
+    val jackson = "2.9.9"
+    val metrics = "4.1.0"
+    val jetty = "9.4.20.v20190813"
+    val kotlinCoroutines = "1.3.0"
+    val guava = "28.1-jre"
 }
