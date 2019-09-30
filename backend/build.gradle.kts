1da695b75451e79abd8ac93a715a602dacce6908
autobumper <autobumper@timgroup.com>
2019-09-30 12:37:54 +0000

    Autobumper: com.timgroup:tim-logger:1.5.1097 -> com.timgroup:tim-logger:1.5.1098  * changes to com.timgroup:tim-logger:    * gallan: gallan: set context on policy before configuring, to avoid spurious error messages to console out.       c.f. https://timgroup.slack.com/archives/C029CE99K/p1569578123087300?thread_ts=1567757314.063800&cid=C029CE99K

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -120,7 +120,7 @@ repositories {
 
 dependencies {
     compile("com.timgroup:Tucker:1.0.1533") // autobump
-    compile("com.timgroup:tim-logger:1.5.1097") // autobump
+    compile("com.timgroup:tim-logger:1.5.1098") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1268") // autobump
     compile("com.timgroup:eventstore-api:0.0.1865") // autobump
     compile("com.timgroup:eventstore-filesystem:0.0.1865") // autobump
