8a2daed723d3b4e62cfda40691215cd698226bcc
autobumper <autobumper@timgroup.com>
2019-11-21 18:42:26 +0000

    Autobumper: com.timgroup:tim-logger:1.5.1098 -> com.timgroup:tim-logger:1.5.1099, com.timgroup:eventstore-filesystem:0.0.1881 -> com.timgroup:eventstore-filesystem:0.0.1882, com.timgroup:tim-structured-events-testing:0.4.1269 -> com.timgroup:tim-structured-events-testing:0.4.1270, com.timgroup:Tucker:1.0.1538 -> com.timgroup:Tucker:1.0.1539, com.timgroup:eventstore-api:0.0.1881 -> com.timgroup:eventstore-api:0.0.1882, com.timgroup:tim-structured-events:0.4.1269 -> com.timgroup:tim-structured-events:0.4.1270  * changes to com.timgroup:tim-logger:    * Steve Haslam: SteveH: Gradle 6  * changes to com.timgroup:eventstore-filesystem:    * Steve Haslam: SteveH: Gradle 6  * changes to com.timgroup:tim-structured-events-testing:    * Steve Haslam: SteveH: Gradle 6  * changes to com.timgroup:Tucker:    * Steve Haslam: SteveH: Gradle 6  * changes to com.timgroup:eventstore-api:    * Steve Haslam: SteveH: Gradle 6  * changes to com.timgroup:tim-structured-events:    * Steve Haslam: SteveH: Gradle 6

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -119,11 +119,11 @@ repositories {
 }
 
 dependencies {
-    compile("com.timgroup:Tucker:1.0.1538") // autobump
-    compile("com.timgroup:tim-logger:1.5.1098") // autobump
-    compile("com.timgroup:tim-structured-events:0.4.1269") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1881") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1881") // autobump
+    compile("com.timgroup:Tucker:1.0.1539") // autobump
+    compile("com.timgroup:tim-logger:1.5.1099") // autobump
+    compile("com.timgroup:tim-structured-events:0.4.1270") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1882") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1882") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:${Versions.metrics}")
@@ -146,7 +146,7 @@ dependencies {
     testCompile(kotlin("test-junit"))
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
-    testCompile("com.timgroup:tim-structured-events-testing:0.4.1269") // autobump
+    testCompile("com.timgroup:tim-structured-events-testing:0.4.1270") // autobump
     testCompile("com.timgroup:eventstore-memory:0.0.1881") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1099") // autobump
