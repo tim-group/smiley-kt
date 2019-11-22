2233f4498dec1fe16df7b761305be72632f89cca
autobumper <autobumper@timgroup.com>
2019-11-22 12:27:59 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1884 -> com.timgroup:eventstore-filesystem:0.0.1885, com.timgroup:eventstore-api:0.0.1884 -> com.timgroup:eventstore-api:0.0.1885, com.timgroup:eventstore-memory:0.0.1884 -> com.timgroup:eventstore-memory:0.0.1885  * changes to com.timgroup:eventstore-filesystem:    * autobumper: Autobumper: com.timgroup:tg-config-java-bindings:1.0.59 -> com.timgroup:tg-config-java-bindings:1.0.60        * changes to com.timgroup:tg-config-java-bindings:          * Steve Haslam: SteveH: Gradle 6  * changes to com.timgroup:eventstore-api:    * autobumper: Autobumper: com.timgroup:tg-config-java-bindings:1.0.59 -> com.timgroup:tg-config-java-bindings:1.0.60        * changes to com.timgroup:tg-config-java-bindings:          * Steve Haslam: SteveH: Gradle 6  * changes to com.timgroup:eventstore-memory:    * autobumper: Autobumper: com.timgroup:tg-config-java-bindings:1.0.59 -> com.timgroup:tg-config-java-bindings:1.0.60        * changes to com.timgroup:tg-config-java-bindings:          * Steve Haslam: SteveH: Gradle 6

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -122,8 +122,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1539") // autobump
     compile("com.timgroup:tim-logger:1.5.1099") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1270") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1884") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1884") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1885") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1885") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:${Versions.metrics}")
@@ -147,7 +147,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1270") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1884") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1885") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1099") // autobump
 
