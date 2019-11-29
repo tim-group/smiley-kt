c5da5732780357635067cc0bb42cf61a22626565
autobumper <autobumper@timgroup.com>
2019-11-29 12:22:31 +0000

    Autobumper: com.timgroup:clocks-testing:1.0.1101 -> com.timgroup:clocks-testing:1.0.1102  * changes to com.timgroup:clocks-testing:    * Steve Haslam: Use Gradle 6's built-in support for sources/javadoc jars

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -149,7 +149,7 @@ dependencies {
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1273") // autobump
     testCompile("com.timgroup:eventstore-memory:0.0.1895") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
-    testCompile("com.timgroup:clocks-testing:1.0.1101") // autobump
+    testCompile("com.timgroup:clocks-testing:1.0.1102") // autobump
 
     runtime("ch.qos.logback:logback-classic:1.2.3")
 
