ec36ec914edef8aaa3b71bfc9f6175f8e51c8b0f
autobumper <autobumper@timgroup.com>
2019-12-10 12:53:16 +0000

    Autobumper: com.timgroup:eventstore-filesystem:0.0.1900 -> com.timgroup:eventstore-filesystem:0.0.1901, com.timgroup:eventstore-api:0.0.1900 -> com.timgroup:eventstore-api:0.0.1901, com.timgroup:eventstore-memory:0.0.1900 -> com.timgroup:eventstore-memory:0.0.1901  * changes to com.timgroup:eventstore-filesystem:    * autobumper: Autobumper: com.timgroup:Tucker:1.0.1540 -> com.timgroup:Tucker:1.0.1541        * changes to com.timgroup:Tucker:          * Alex Conlin-Oakley: Andy/Alex: deprecate constructor without MetricsRegistry             Without passing a MetricsRegistry Tucker will use a default, empty MetricsRegistry and Tucker won't publish any metrics to Prometheus  * changes to com.timgroup:eventstore-api:    * autobumper: Autobumper: com.timgroup:Tucker:1.0.1540 -> com.timgroup:Tucker:1.0.1541        * changes to com.timgroup:Tucker:          * Alex Conlin-Oakley: Andy/Alex: deprecate constructor without MetricsRegistry             Without passing a MetricsRegistry Tucker will use a default, empty MetricsRegistry and Tucker won't publish any metrics to Prometheus  * changes to com.timgroup:eventstore-memory:    * autobumper: Autobumper: com.timgroup:Tucker:1.0.1540 -> com.timgroup:Tucker:1.0.1541        * changes to com.timgroup:Tucker:          * Alex Conlin-Oakley: Andy/Alex: deprecate constructor without MetricsRegistry             Without passing a MetricsRegistry Tucker will use a default, empty MetricsRegistry and Tucker won't publish any metrics to Prometheus

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -122,8 +122,8 @@ dependencies {
     compile("com.timgroup:Tucker:1.0.1541") // autobump
     compile("com.timgroup:tim-logger:1.5.1100") // autobump
     compile("com.timgroup:tim-structured-events:0.4.1274") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1900") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1900") // autobump
+    compile("com.timgroup:eventstore-api:0.0.1901") // autobump
+    compile("com.timgroup:eventstore-filesystem:0.0.1901") // autobump
     compile(kotlin("stdlib-jdk8"))
     compile(kotlin("reflect"))
     compile("io.dropwizard.metrics:metrics-core:${Versions.metrics}")
@@ -147,7 +147,7 @@ dependencies {
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
     testCompile("com.timgroup:tim-structured-events-testing:0.4.1274") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1900") // autobump
+    testCompile("com.timgroup:eventstore-memory:0.0.1901") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1102") // autobump
 
