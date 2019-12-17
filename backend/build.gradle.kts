ccafd42ec220a71a8953a48cf93ce034545a8c52
autobumper <autobumper@timgroup.com>
2019-12-17 15:53:16 +0000

    Autobumper: com.timgroup:tim-structured-events-testing:0.4.1275 -> com.timgroup:tim-structured-events-testing:0.4.1276, com.timgroup:tim-structured-events:0.4.1275 -> com.timgroup:tim-structured-events:0.4.1276  * changes to com.timgroup:tim-structured-events-testing:    * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1103 -> com.timgroup:clocks-testing:1.0.1104        * changes to com.timgroup:clocks-testing:          * Steve Haslam: Bump Joda-Time to 2.10.5  * changes to com.timgroup:tim-structured-events:    * autobumper: Autobumper: com.timgroup:clocks-testing:1.0.1103 -> com.timgroup:clocks-testing:1.0.1104        * changes to com.timgroup:clocks-testing:          * Steve Haslam: Bump Joda-Time to 2.10.5

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -121,7 +121,7 @@ repositories {
 dependencies {
     compile("com.timgroup:Tucker:1.0.1542") // autobump
     compile("com.timgroup:tim-logger:1.5.1101") // autobump
-    compile("com.timgroup:tim-structured-events:0.4.1275") // autobump
+    compile("com.timgroup:tim-structured-events:0.4.1276") // autobump
     compile("com.timgroup:eventstore-api:0.0.1907") // autobump
     compile("com.timgroup:eventstore-filesystem:0.0.1907") // autobump
     compile(kotlin("stdlib-jdk8"))
@@ -146,7 +146,7 @@ dependencies {
     testCompile(kotlin("test-junit"))
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
-    testCompile("com.timgroup:tim-structured-events-testing:0.4.1275") // autobump
+    testCompile("com.timgroup:tim-structured-events-testing:0.4.1276") // autobump
     testCompile("com.timgroup:eventstore-memory:0.0.1907") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1104") // autobump
