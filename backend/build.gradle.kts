2c94e82db39829310dd63ece49a53993feb3be56
autobumper <autobumper@timgroup.com>
2020-01-29 10:09:52 +0000

    Autobumper: com.timgroup:tim-structured-events-testing:0.4.1278 -> com.timgroup:tim-structured-events-testing:0.4.1279, com.timgroup:tim-structured-events:0.4.1278 -> com.timgroup:tim-structured-events:0.4.1279  * changes to com.timgroup:tim-structured-events-testing:    * Steve Haslam: SteveH: move LocalEventSink and StructuredEventMatcher to subpackage and specify automatic module names  * changes to com.timgroup:tim-structured-events:    * Steve Haslam: SteveH: move LocalEventSink and StructuredEventMatcher to subpackage and specify automatic module names

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -119,7 +119,7 @@ repositories {
 dependencies {
     compile("com.timgroup:Tucker:1.0.1544") // autobump
     compile("com.timgroup:tim-logger:1.5.1101") // autobump
-    compile("com.timgroup:tim-structured-events:0.4.1278") // autobump
+    compile("com.timgroup:tim-structured-events:0.4.1279") // autobump
     compile("com.timgroup:eventstore-api:0.0.1926") // autobump
     compile("com.timgroup:eventstore-filesystem:0.0.1926") // autobump
     compile(kotlin("stdlib-jdk8"))
@@ -144,7 +144,7 @@ dependencies {
     testCompile(kotlin("test-junit"))
     testCompile("com.natpryce:hamkrest:1.4.2.2")
     testCompile("org.araqnid:hamkrest-json:1.0.3")
-    testCompile("com.timgroup:tim-structured-events-testing:0.4.1278") // autobump
+    testCompile("com.timgroup:tim-structured-events-testing:0.4.1279") // autobump
     testCompile("com.timgroup:eventstore-memory:0.0.1926") // autobump
     testCompile("org.apache.httpcomponents:httpclient:4.5.5")
     testCompile("com.timgroup:clocks-testing:1.0.1105") // autobump
