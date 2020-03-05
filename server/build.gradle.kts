25b78f28c1e3822e95f447b34fdefede2748220f
Steve Haslam <steve.haslam@timgroup.com>
2020-03-05 10:45:34 +0000

    SteveH: replace compile etc dependencies with implementation etc

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -117,39 +117,39 @@ repositories {
 }
 
 dependencies {
-    compile("com.timgroup:Tucker:1.0.1546") // autobump
-    compile("com.timgroup:tim-logger:1.5.1101") // autobump
-    compile("com.timgroup:tim-structured-events:0.4.1281") // autobump
-    compile("com.timgroup:eventstore-api:0.0.1948") // autobump
-    compile("com.timgroup:eventstore-filesystem:0.0.1948") // autobump
-    compile(kotlin("stdlib-jdk8"))
-    compile(kotlin("reflect"))
-    compile("io.dropwizard.metrics:metrics-core:${Versions.metrics}")
-    compile("io.dropwizard.metrics:metrics-jvm:${Versions.metrics}")
-    compile("io.dropwizard.metrics:metrics-graphite:${Versions.metrics}")
-    compile("io.dropwizard.metrics:metrics-jetty9:${Versions.metrics}")
-    compile("org.eclipse.jetty:jetty-server:${Versions.jetty}")
-    compile("org.eclipse.jetty:jetty-servlet:${Versions.jetty}")
-    compile("com.fasterxml.jackson.core:jackson-databind:${Versions.jackson}")
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}")
-    compile("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jackson}")
-    compile("com.fasterxml.jackson.jaxrs:jackson-jaxrs-json-provider:${Versions.jackson}")
-    compile("com.google.guava:guava:${Versions.guava}")
-    compile("org.jboss.resteasy:resteasy-jaxrs:3.1.2.Final")
-    compile("javax.mail:mail:1.4.6")
-    compile("org.apache.commons:commons-compress:1.16.1")
-    compile("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.10")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${Versions.kotlinCoroutines}")
+    implementation("com.timgroup:Tucker:1.0.1546") // autobump
+    implementation("com.timgroup:tim-logger:1.5.1101") // autobump
+    implementation("com.timgroup:tim-structured-events:0.4.1281") // autobump
+    implementation("com.timgroup:eventstore-api:0.0.1948") // autobump
+    implementation("com.timgroup:eventstore-filesystem:0.0.1948") // autobump
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("io.dropwizard.metrics:metrics-core:${Versions.metrics}")
+    implementation("io.dropwizard.metrics:metrics-jvm:${Versions.metrics}")
+    implementation("io.dropwizard.metrics:metrics-graphite:${Versions.metrics}")
+    implementation("io.dropwizard.metrics:metrics-jetty9:${Versions.metrics}")
+    implementation("org.eclipse.jetty:jetty-server:${Versions.jetty}")
+    implementation("org.eclipse.jetty:jetty-servlet:${Versions.jetty}")
+    implementation("com.fasterxml.jackson.core:jackson-databind:${Versions.jackson}")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jackson}")
+    implementation("com.fasterxml.jackson.jaxrs:jackson-jaxrs-json-provider:${Versions.jackson}")
+    implementation("com.google.guava:guava:${Versions.guava}")
+    implementation("org.jboss.resteasy:resteasy-jaxrs:3.1.2.Final")
+    implementation("javax.mail:mail:1.4.6")
+    implementation("org.apache.commons:commons-compress:1.16.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.10")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${Versions.kotlinCoroutines}")
 
-    testCompile(kotlin("test-junit"))
-    testCompile("com.natpryce:hamkrest:1.4.2.2")
-    testCompile("org.araqnid:hamkrest-json:1.0.3")
-    testCompile("com.timgroup:tim-structured-events-testing:0.4.1281") // autobump
-    testCompile("com.timgroup:eventstore-memory:0.0.1948") // autobump
-    testCompile("org.apache.httpcomponents:httpclient:4.5.5")
-    testCompile("com.timgroup:clocks-testing:1.0.1105") // autobump
+    testImplementation(kotlin("test-junit"))
+    testImplementation("com.natpryce:hamkrest:1.4.2.2")
+    testImplementation("org.araqnid:hamkrest-json:1.0.3")
+    testImplementation("com.timgroup:tim-structured-events-testing:0.4.1281") // autobump
+    testImplementation("com.timgroup:eventstore-memory:0.0.1948") // autobump
+    testImplementation("org.apache.httpcomponents:httpclient:4.5.5")
+    testImplementation("com.timgroup:clocks-testing:1.0.1105") // autobump
 
-    runtime("ch.qos.logback:logback-classic:1.2.3")
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
 
     web(project(path = ":webui", configuration = "web"))
 }
