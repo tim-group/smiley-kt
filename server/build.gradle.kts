703b50d94cf89495ec64fd31d6346701d52f99cb
autobumper <autobumper@timgroup.com>
2020-06-23 12:03:46 +0000

    Autobumper: com.timgroup:Tucker:1.0.1553 -> com.timgroup:Tucker:1.0.1554  * changes to com.timgroup:Tucker:    * mpimm: mpimm: Add static methods that create an ApplicationInformationServer with Metrics rather than MetricRegistry

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -117,7 +117,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.timgroup:Tucker:1.0.1553") // autobump
+    implementation("com.timgroup:Tucker:1.0.1554") // autobump
     implementation("com.timgroup:tim-logger:1.5.1106") // autobump
     implementation("com.timgroup:tim-structured-events:0.4.1284") // autobump
     implementation("com.timgroup:eventstore-api:0.0.2021") // autobump
