24461c40b4e822301d71bd2bbc6a872594fd2478
autobumper <autobumper@timgroup.com>
2020-06-26 13:03:27 +0000

    Autobumper: com.timgroup:Tucker:1.0.1555 -> com.timgroup:Tucker:1.0.1556  * changes to com.timgroup:Tucker:    * autobumper: Autobumper: com.timgroup:tim-metrics:1.0.21 -> com.timgroup:tim-metrics:1.0.22        * unable to get details for changes to com.timgroup:tim-metrics

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -117,7 +117,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.timgroup:Tucker:1.0.1555") // autobump
+    implementation("com.timgroup:Tucker:1.0.1556") // autobump
     implementation("com.timgroup:tim-logger:1.5.1106") // autobump
     implementation("com.timgroup:tim-structured-events:0.4.1284") // autobump
     implementation("com.timgroup:eventstore-api:0.0.2025") // autobump
