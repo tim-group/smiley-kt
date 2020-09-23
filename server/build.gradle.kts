ca236bfff431def900a1aeefdbf39614bc4476f5
autobumper <autobumper@timgroup.com>
2020-09-23 11:09:55 +0000

    Autobumper: com.timgroup:Tucker:1.0.1569 -> com.timgroup:Tucker:1.0.1570  * changes to com.timgroup:Tucker:    * Waseem Taj: wtaj - Introducing new constructors for ApplicationInformationHandler and ApplicationInformationServlet that take in Metrics object.       this is in preparation for the next step where we want to publish metrics on port 8001 including status page metrics.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -117,7 +117,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.timgroup:Tucker:1.0.1569") // autobump
+    implementation("com.timgroup:Tucker:1.0.1570") // autobump
     implementation("com.timgroup:tim-logger:1.5.1108") // autobump
     implementation("com.timgroup:tim-structured-events:0.4.1284") // autobump
     implementation("com.timgroup:eventstore-api:0.0.2055") // autobump
